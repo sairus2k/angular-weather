8c4b72d9a2bf9f694a247f8aed3e3d15850d6dce
Anton Savoskin <anton@sliday.com>
2016-12-15 22:32:45 +0700

    Add nav component

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aw-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
