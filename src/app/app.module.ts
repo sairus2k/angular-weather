5d14bf848d14d940de8602cbaafc5af9d130a994
Anton Savoskin <anton@sliday.com>
2016-12-28 18:37:03 +0700

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -9,6 +10,11 @@ import { WeatherComponent } from './weather/weather.component';
 import { WeatherFormComponent } from './weather-form/weather-form.component';
 import { ErrorModalComponent } from './error-modal/error-modal.component';
 import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  {path: '', component: WeatherComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
@@ -21,6 +27,7 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule,
     HttpModule
   ],
