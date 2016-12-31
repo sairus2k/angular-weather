450b5407722dc0e20b9b52858e06046b2a020ea6
Anton Savoskin <anton@sliday.com>
2016-12-31 10:22:59 +0700

    Add search on queryParams changes

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { OpenWeatherMapService } from '../open-weather-map.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'aw-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.scss']
 })
-export class WeatherComponent implements OnInit {
+export class WeatherComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
   errorMessage: any = null;
   isLoading: boolean = false;
   location: string = '';
   temp: number = 0;
 
-  constructor(private openWeatherMapService: OpenWeatherMapService) {
+  constructor(private openWeatherMapService: OpenWeatherMapService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
+    this.subscription = this.activatedRoute.queryParams.subscribe(
+      (queryParams: any) => {
+        const location = queryParams['location'];
+        if (location && location.length > 0) {
+          this.router.navigate(['/']).then(() => {
+            this.handleSearch(location);
+          });
+        }
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   handleSearch(location) {
