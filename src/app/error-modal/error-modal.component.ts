e65e4c62773d9848701427c09739f47a30c643ce
Anton Savoskin <anton@sliday.com>
2016-12-31 10:00:10 +0700

    Fix error-modal message

--- src/app/error-modal/error-modal.component.ts
+++ src/app/error-modal/error-modal.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./error-modal.component.scss']
 })
 export class ErrorModalComponent implements OnInit {
+  @Input() errorTitle: string = 'Error';
   @Input() errorMessage;
 
   constructor() { }
