8869305f70f18b3c7cc7655a797f32bb69fb7a5d
Anton Savoskin <anton@sliday.com>
2016-12-31 12:12:01 +0700

    Fix OpenWeatherMap api url

--- src/app/open-weather-map.service.ts
+++ src/app/open-weather-map.service.ts
@@ -5,7 +5,8 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class OpenWeatherMapService {
-  private OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=d5a17281219749675b7897dce29931b3&units=metric';
+  private OPEN_WEATHER_MAP_URL = 'api.openweathermap.org/data/2.5/weather';
+  private APP_ID = 'd5a17281219749675b7897dce29931b3';
 
   constructor(private http: Http) {
   }
@@ -13,6 +14,8 @@ export class OpenWeatherMapService {
   getTemp(location: string) {
     const params: URLSearchParams = new URLSearchParams();
     params.set('q', location);
+    params.set('appid', this.APP_ID);
+    params.set('units', 'metric');
     return this.http
       .get(this.OPEN_WEATHER_MAP_URL, {search: params})
       .toPromise()
